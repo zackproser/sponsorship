a8351bc07fcfe89e5d5eac9c6a6dff2fd8ff5608
Zachary Proser <zackproser@gmail.com>
2024-05-03 16:41:36 -0400

    Install Google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,8 @@
 import type { Metadata } from "next";
+
 import { Inter } from "next/font/google";
+import { GoogleAnalytics } from '@next/third-parties/google'
+
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -16,7 +19,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {children}
+        <GoogleAnalytics gaId="G-DFX9S1FRMB" />
+      </body>
     </html>
   );
 }
